205626b0de1b7a2ba384ce10d68f2d79df24097d
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-09-27 21:53:27 +0700

    first commit

---
+++ Player Test (Sprite by Joalor64GH) on LUA/data/playerInit.lua
@@ -0,0 +1,15 @@
+function onCreate() 
+    makeText("text", 0, 0, 0, "FNF Boyfriend Animation Test", 12);
+    addText("text");
+
+    makeAnimationSprite("player", 0, 0, "player")
+    addAnimationByPrefix("player", "idle", "Idle", 24, false)
+    addAnimationByPrefix("player", "down", "Down0", 24, false)
+    addAnimationByPrefix("player", "left", "Left0", 24, false)
+    addAnimationByPrefix("player", "right", "Right0", 24, false)
+    addAnimationByPrefix("player", "up", "Up0", 24, false)
+
+    setSpriteProperty("player", "antialiasing", true)
+
+    addSprite("player")
+end
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Game-Hscript-Template
+# Template Code for Game-Hsscript
+checked sum on other template i set from here!
+
+is funni
